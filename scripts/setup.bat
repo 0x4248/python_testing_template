646c339b715b265f79c4b33a609e73e30cd5f469
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-07-06 18:44:08 +0100

    Edit echo messages
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,5 +1,8 @@
 @echo off
 echo Setting up virtual environment
 python -m venv .
-echo Done. Now run pip install -r scripts\requirements\requirements.txt to install dependencies
-echo or pip install -r scripts\requirements\top_ten.txt to install only the top ten python packages
+echo Done. Now run the command
+echo pip install -r scripts\requirements\requirements.txt 
+echo to install dependencies or run
+echo pip install -r scripts\requirements\top_ten.txt 
+echo to install only the top ten python packages
